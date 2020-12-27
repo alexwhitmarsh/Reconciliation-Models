80398ae66cac534c18914819dbf942365bbd4b56
alexwhitmarsh <alexwhitmarsh@yahoo.co.uk>
2020-12-27 08:16:05 +0000

    PR19 In Period Adjustment Model 200614 2

--- Model Calculations/PR19 in period adjustment model.py
+++ Model Calculations/PR19 in period adjustment model.py
@@ -7,22 +7,27 @@ import numpy as np
 
 df = pd.DataFrame({
                                     'FY':   [2020,     2021,    2022,   2023,   2024,   2025],
-#'Allowed_revenue_starting_point_in_FD':    [100,      0,       0,      0,      0,      0],
-                             'K_factors':   [0,        0.03,   0.03,    0.03,   0.03,   0.03],
-                              'CPIH':       [0.02,     0.02,   0.02,    0.02,   0.02,   0.02],
-                 'Marginal_tax_rate':       [0,        0,       0,      0.19,   0.19,   0.19]
+             'Water_resources_K_factors':   [0,        0.03,   0.03,    0.03,   0.03,   0.03],
+               'Water_network_K_factors':   [0,        0.04,   0.04,    0.04,   0.04,   0.04],
+          'Wastewater_network_K_factors':   [0,        0.05,   0.05,    0.05,   0.05,   0.05],
+                                  'CPIH':   [0.02,     0.02,   0.02,    0.02,   0.02,   0.02],
+                     'Marginal_tax_rate':   [0,        0,       0,      0.19,   0.19,   0.19]
 })
 df.set_index('FY', drop=False, inplace=True)
 
 # Generate the culmulative series based on CPIH + K
-lst = []
-z = 1
-for FY, K, CPIH in zip(df.FY, df.K_factors, df.CPIH):
-    if FY > 2020:
-        z = z * (1 + K + CPIH)
-    else: z = 1
-    lst.append(z)
-df['CPIH_K_culmulative'] = lst
+K_factors = [df.Water_resources_K_factors, df.Water_network_K_factors, df.Wastewater_network_K_factors]
+
+for i in K_factors:
+    lst = []
+    z = 1
+    for FY, K, CPIH in zip(df.FY, i, df.CPIH):
+        if FY > 2020:
+            z = z * (1 + K + CPIH)
+        else: z = 1
+        lst.append(z)
+    df[str(i.name) + '_CPIH_K_culmulative'] = lst
+print(df.tail())
 
 # Generate the culmulative series based on CPIH
 lst = []
@@ -33,8 +38,6 @@ for FY, CPIH in zip(df.FY, df.CPIH):
     else: z = 1
     lst.append(z)
 df['CPIH_culmulative'] = lst
-
-print(lst)
 
 
 #  Scalars
@@ -50,44 +53,35 @@ Dummy_control = 16
 
 Reporting_year = 2021
 
-
-
 #  WATER RESOURCES
 
 # ODI PAYMENT
 
-Year_of_adjustment_to_be_applied = Reporting_year + 2
-df['ODI_payment_for_this_control'] = df.FY.apply(lambda x: Water_resources
-                                                 if x == Year_of_adjustment_to_be_applied
-                                                 else 0)
+# Sets
 
-# REVENUE ADJUSTMENTS
+Starting_revenue = [Water_resources, Water_network_plus, Wastewater_network_plus]
 
-df['Allowed_revenue'] = Allowed_revenue_starting_point_in_FD * df.CPIH_K_culmulative
 
-df['ODI_value_nominal_price'] = df.ODI_payment_for_this_control * df.CPIH_culmulative
+def poop(control, K_factor):
+    global Year_of_adjustment_to_be_applied
+    Year_of_adjustment_to_be_applied = Reporting_year + 2
+    df['ODI_payment_for_this_control'] = df.FY.apply(lambda x: control # Water_resources
+    if x == Year_of_adjustment_to_be_applied
+    else 0)
+    # REVENUE ADJUSTMENTS
+    df['Allowed_revenue'] = Allowed_revenue_starting_point_in_FD * df.CPIH_K_culmulative
+    df['ODI_value_nominal_price'] = df.ODI_payment_for_this_control * df.CPIH_culmulative
+    # Tax adjustment
+    df['Total_value_of_ODI'] = df.ODI_value_nominal_price * (1 / (1 - df.Marginal_tax_rate))
+    df['Revised_total_nominal_revenue'] = df.Allowed_revenue + df.Total_value_of_ODI
+    # Revised K
+    df['Allowed_revenue_percentage_movement'] = df.Revised_total_nominal_revenue.pct_change()
+    df['Allowed_revenue_percentage_movement_deflated'] = df.apply(lambda row: row.Allowed_revenue_percentage_movement
+    if row.FY >= Year_of_adjustment_to_be_applied
+    else 0, axis=1)
+    df['Revised_K'] = df.Allowed_revenue_percentage_movement_deflated.round(2)  # this is simplyfied - the original calc did odd rounding up or down
 
-# Tax adjustment
 
-df['Tax_on_geogetric_uplift'] = 1 / ( 1 - df.Marginal_tax_rate) - 1
-
-df['Tax_on_nominal_ODI'] = df.ODI_value_nominal_price * df.Tax_on_geogetric_uplift
-
-df['Total_value_of_ODI'] =  df.ODI_value_nominal_price + df.Tax_on_nominal_ODI
-
-df['Revised_total_nominal_revenue'] = df.Allowed_revenue + df.Total_value_of_ODI
-
-# Revised K
-
-df['Allowed_revenue_percentage_movement'] = df.Revised_total_nominal_revenue.pct_change()
-
-df['Year_that_price_limits_should_be_recalculated'] = df.FY.apply(lambda x: 1
-                                                               if x >= Year_of_adjustment_to_be_applied
-                                                               else 0)
-
-df['Allowed_revenue_percentage_movement_deflated'] = (df.Allowed_revenue_percentage_movement - CPIH) 
-
-df['Revised_K'] = df.Allowed_revenue_percentage_movement_deflated.round(2) # this is simplyfied - the original calc did odd rounding up or down
-
+poop(Water_resources)
 
 print(df.tail())
