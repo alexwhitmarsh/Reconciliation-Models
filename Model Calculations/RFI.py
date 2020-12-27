2507a0ef4962bef6920ead3adec56e978d40fefa
alexwhitmarsh <alexwhitmarsh@yahoo.co.uk>
2020-12-27 07:59:07 +0000

    RFI 200619

--- Model Calculations/RFI.py
+++ Model Calculations/RFI.py
@@ -1,58 +1,48 @@
 import pandas as pd
 
 df_global = pd.DataFrame({
-                                    'FY':   [2020,     2021,    2022,   2023,   2024,   2025],
-                                    'CPIH':   [0.021,     0.015,   0.02,    0.02,   0.02,   0.02],
+                            ('All', 'FY'):   [2020,     2021,    2022,   2023,   2024,   2025],
+                            ('All', 'CPIH'): [0.021,     0.015,   0.02,    0.02,   0.02,   0.02],
      })
-df_global.set_index('FY', drop=False, inplace=True)
+df_global.set_index(('All', 'FY'), drop=False, inplace=True)
 
 # Generate the culmulative series based on CPIH
 lst = []
 z = 1
-for FY, CPIH in zip(df_global.FY, df_global.CPIH):
+for FY, CPIH in zip(df_global[('All', 'FY')], df_global[('All', 'CPIH')]):
     if FY > 2020:
         z = z * (1 + CPIH)
     else: z = 1
     lst.append(z)
-df_global['CPIH_culmulative'] = lst
+df_global[('All', 'CPIH_culmulative')] = lst
 
-df_global['CPIH_delta_shift1'] = df_global.CPIH.shift(-1)
-df_global['CPIH_delta_shift2'] = df_global.CPIH.shift(-2)
+df_global[('All', 'CPIH_delta_shift1')] = df_global[('All', 'CPIH')].shift(-1)
+df_global[('All', 'CPIH_delta_shift2')] = df_global[('All', 'CPIH')].shift(-2)
 
-
-
-print(df_global)
 
 df = pd.DataFrame({
-                                    'FY':   [2020,     2021,    2022,   2023,   2024,   2025],
-             'Water_resources_K_factors':   [0,        0.03,   0.03,    0.03,   0.03,   0.03],
-               'Water_network_K_factors':   [0,        0.04,   0.04,    0.04,   0.04,   0.04],
-          'Wastewater_network_K_factors':   [0,        0.05,   0.05,    0.05,   0.05,   0.05],
-     'Wastewater_Network_Actual_revenue':   [0,        110,    120,     130,    140,    150],
-          'Water_Network_Actual_revenue':   [0,        110,    120,     130,    140,    150],
-        'Water_resources_Actual_revenue':   [0,        110,    120,     130,    140,    150],
-'Wastewater_Network_Blind_year_profiling_factor':   [0,        0,      0.25,    0.25,   0.25,   0.25],
-'Water_Network_Blind_year_profiling_factor':        [0,        0,      0.25,    0.25,   0.25,   0.25],
-'Water_resources_Blind_year_profiling_factor':      [0,        0,      0.25,    0.25,   0.25,   0.25]
+    ('All', 'FY'):                                          [2020,     2021,    2022,   2023,   2024,   2025],
+    ('Water_resources', 'Bilateral_entry_adjustment'):      [0,        0,      0,       -3,     -3,       -3],
+    ('Water_resources', 'K_factors'):                       [0,        0.03,   0.03,    0.03,   0.03,   0.03],
+    ('Water_network', 'K_factors'):                         [0,        0.04,   0.04,    0.04,   0.04,   0.04],
+    ('Wastewater_network', 'K_factors'):                    [0,        0.05,   0.05,    0.05,   0.05,   0.05],
+    ('Wastewater_network', 'Actual_revenue'):               [0,        110,    120,     130,    140,    150],
+    ('Water_network', 'Actual_revenue'):                    [0,        55,     60,      65,     70,     75],
+    ('Water_resources', 'Actual_revenue'):                  [0,        55,     60,      65,     70,     75],
+    ('Wastewater_network', 'Blind_year_profiling_factor'):  [0,        0,      0.25,    0.25,   0.25,   0.25],
+    ('Water_network', 'Blind_year_profiling_factor'):       [0,        0,      0.25,    0.25,   0.25,   0.25],
+    ('Water_resources', 'Blind_year_profiling_factor'):     [0,        0,      0.25,    0.25,   0.25,   0.25],
+('Water_network', 'Proportion_of_penalty_allocated_to_WaterN'): [0,    0.66,    0.66,     0.66,    0.66,    0.66],
 })
-
-df2 = pd.DataFrame({
-    ('All', 'FY'):   [2020,     2021,    2022,   2023,   2024,   2025],
-    ('Water_resources', 'K_factors'):   [0,        0.03,   0.03,    0.03,   0.03,   0.03],
-    ('Water_network_K', 'factors'):   [0,        0.04,   0.04,    0.04,   0.04,   0.04],
-    ('Wastewater_network', 'K_factors'):   [0,        0.05,   0.05,    0.05,   0.05,   0.05],
-    ('Wastewater_Network', 'Actual_revenue'):   [0,        110,    120,     130,    140,    150],
-    ('Water_Network_Actual', 'revenue'):   [0,        110,    120,     130,    140,    150],
-    ('Water_resources', 'Actual_revenue'):   [0,        110,    120,     130,    140,    150],
-    ('Wastewater_Network', 'Blind_year_profiling_factor'):   [0,        0,      0.25,    0.25,   0.25,   0.25],
-    ('Water_Network', 'Blind_year_profiling_factor'):        [0,        0,      0.25,    0.25,   0.25,   0.25],
-    ('Water_resources', 'Blind_year_profiling_factor'):      [0,        0,      0.25,    0.25,   0.25,   0.25]
-})
-df2.set_index(('All', 'FY'), drop=False, inplace=True)
+df.set_index(('All', 'FY'), drop=False, inplace=True)
+df.sort_index(axis=1, inplace=True)
 
 
-print(df2.transpose())
 
+
+# MERGE LOCAL AND GLOBAL DATASETS
+
+df = df.merge(df_global,left_index=True, right_index=True, suffixes=('_drop', "")).drop(columns=('All_drop', 'FY'))
 
 
 #  SCALARS
@@ -73,11 +63,12 @@ Water_resources_total_blind_year_adjustment = 15
 
 
 # SETS
-controls = ['Wastewater_Network', 'Water_Network', 'Water_resources']
+controls = ['Wastewater_network', 'Water_network', 'Water_resources']
+wholesale_controls = ['Wholesale_water', 'Wastewater_network']
 allowed_revenues = [Wastewater_Network_allowed_revenue, Water_Network_allowed_revenue, Water_resources_allowed_revenue]
 total_blind_year_adjustments = [Wastewater_Network_total_blind_year_adjustment, Water_Network_total_blind_year_adjustment,
                                 Water_resources_total_blind_year_adjustment]
-K_factors = [df.Wastewater_network_K_factors, df.Water_network_K_factors, df.Water_resources_K_factors]
+K_factors = [df[('Wastewater_network', 'K_factors')], df[('Water_network', 'K_factors')], df[('Water_resources', 'K_factors')]]
 
 
 # CALCULATIONS
@@ -85,86 +76,137 @@ K_factors = [df.Wastewater_network_K_factors, df.Water_network_K_factors, df.Wat
 # This creates the allowed revenues and blind year adjustents for each control
 
 
-for control, AR, total_blind_year_adjustment, i in zip(controls, allowed_revenues, total_blind_year_adjustments, K_factors):
+for control, AR, total_blind_year_adjustment, K_factor in zip(controls, allowed_revenues, total_blind_year_adjustments, K_factors):
     lst = []
     z = 1
-    for FY, K, CPIH in zip(df.FY, i, df.CPIH):
+    for FY, K, CPIH in zip(df[('All', 'FY')], K_factor, df[('All', 'CPIH')]):
         if FY > 2020:
             z = z * (1 + K + CPIH)
         else:
             z = 1
         lst.append(z)
-    df[control + '_CPIH_K_culmulative'] = lst
+    df[(control, 'CPIH_K_culmulative')] = lst
 
 
-    df[control + '_Allowed_revenue'] = df.apply(lambda row: AR * row[control + '_CPIH_K_culmulative']
-                                 if row.FY > 2020
+    df[(control, 'Allowed_revenue')] = df.apply(lambda row: AR * row[(control, 'CPIH_K_culmulative')]
+                                 if row[('All', 'FY')] > 2020
                                  else 0, axis=1)
 
+    df[(control, 'Blind_year_adjustment_profiled')] = df[(control, 'Blind_year_profiling_factor')] \
+                                                      * total_blind_year_adjustment
 
-    df[control + '_Blind_year_adjustment_profiled'] = df[control + '_Blind_year_profiling_factor'] * total_blind_year_adjustment
+    df[(control, 'Blind_year_adjustment_inc_financing_rate')] = df[(control, 'Blind_year_adjustment_profiled')] * \
+                                                 (1 + discount_rate) ** (df[('All', 'FY')] - 2020)
 
-    df[control + '_Blind_year_adjustment_inc_financing_rate'] = df[control + '_Blind_year_adjustment_profiled'] * \
-                                                 (1 + discount_rate) ** (df.FY- 2020)
+    df[(control, 'Blind_year_adjustment_inc_financing_inflation')] = df[(control, 'Blind_year_adjustment_inc_financing_rate')] * \
+                                                        df[('All', 'CPIH_culmulative')]
 
-    df[control + '_Blind_year_adjustment_inc_financing_inflation'] = df[control + '_Blind_year_adjustment_inc_financing_rate'] * \
-                                                        df['CPIH_culmulative']
-
-print(df.transpose())
 
 # REVENUE ADJUSTMENT
 
-df['RFI'] = 0
+df.loc[:, ('Water_resources', 'Revenue_imbalance')] = 0
 
-"""
-for i in df.FY:
-    if i > 2020:
-        df.loc[i, 'Adjusted_allowed_revenue'] = df.loc[i, 'Wastewater_Network_Allowed_revenue'] +\
-                                         df.loc[i, 'Blind_year_adjustment_inc_financing_inflation'] +\
-                                         df.loc[i, 'RFI']
+for control in controls:
+    df[(control, 'RFI')] = 0
+    df[(control, 'Revenue_imbalance')] = 0
+    df[(control, 'Adjusted_allowed_revenue')] = 0
+    df[(control, 'Main_revenue_adjustment_with_financing')] = 0
+    df[(control, 'Main_revenue_adjustment_financing_inflation')] = 0
+    df[('Wholesale_water', 'Revenue_imbalance')] = 0
+    df[('Wholesale_water', 'Adjusted_allowed_revenue')] = 0
 
-        df.loc[i, 'Revenue_imbalance'] = df.loc[i, 'Adjusted_allowed_revenue'] - df.loc[i, 'Actual_revenue']
 
-        df.loc[i, 'Main_revenue_adjustment_with_financing'] = df.loc[i, 'Revenue_imbalance'] * ((1 + discount_rate)**2)
+for year in df[('All', 'FY')]:
+    if year > 2020:
+        for control in controls:
+            if ('network' in control):
+                df.loc[year, (control, 'Adjusted_allowed_revenue')] = df.loc[year, (control, 'Allowed_revenue')] + \
+                                                                      df.loc[year, (control, 'Blind_year_adjustment_inc_financing_inflation')] + \
+                                                                      df.loc[year, (control, 'RFI')]
+            else:
+                df.loc[year, (control, 'Adjusted_allowed_revenue')] = df.loc[year, (control, 'Allowed_revenue')] + \
+                                                                      df.loc[year, (control, 'Blind_year_adjustment_inc_financing_inflation')] + \
+                                                                      df.loc[year, (control, 'RFI')] + \
+                                                                      df.loc[year, ('Water_resources', 'Bilateral_entry_adjustment')]
 
-        df.loc[i, 'Main_revenue_adjustment_financing_inflation'] = df.loc[i, 'Main_revenue_adjustment_with_financing'] *\
-                                                            (1 + df.loc[i, 'CPIH_delta_shift1']) *\
-                                                            (1 + df.loc[i, 'CPIH_delta_shift2'])
+            df.loc[year, (control, 'Revenue_imbalance')] = df.loc[year, (control, 'Adjusted_allowed_revenue')] - \
+                                                           df.loc[year, (control, 'Actual_revenue')]
 
-        df.loc[i, 'Forecast_error'] = abs(df.loc[i, 'Revenue_imbalance']) / df.loc[i, 'Adjusted_allowed_revenue']
+            df.loc[year, (control, 'Main_revenue_adjustment_with_financing')] = df.loc[year, (control, 'Revenue_imbalance')] * \
+                                                                                ((1 + discount_rate)**2)
 
-        df.loc[i, 'Penalty_applicable'] = 1 if df.loc[i, 'Forecast_error'] > Minimum_threshold else 0
+            df.loc[year, (control, 'Main_revenue_adjustment_financing_inflation')] = df.loc[year, (control, 'Main_revenue_adjustment_with_financing')] * \
+                                                                                     (1 + df.loc[year, ('All', 'CPIH_delta_shift1')]) * \
+                                                                                     (1 + df.loc[year, ('All', 'CPIH_delta_shift2')])
 
-        df.loc[i, 'Error_magnitude'] = (df.loc[i,'Forecast_error'] - Minimum_threshold) / \
-                                (Maximum_threshold - Minimum_threshold) \
-                                if df.loc[i, 'Penalty_applicable'] == 1 \
+        df.loc[year, ('Wholesale_water', 'Revenue_imbalance')] = df.loc[year, ('Water_resources', 'Revenue_imbalance')] + \
+                                                                 df.loc[year, ('Water_network', 'Revenue_imbalance')]
+
+        df.loc[year, ('Wholesale_water', 'Adjusted_allowed_revenue')] = df.loc[year, ('Water_resources', 'Adjusted_allowed_revenue')] + \
+                                                                        df.loc[year, ('Water_network', 'Adjusted_allowed_revenue')] \
+
+
+    for wholesale_control in wholesale_controls:
+            df.loc[year, (wholesale_control, 'Forecast_error')] = abs(df.loc[year, (wholesale_control, 'Revenue_imbalance')]) / \
+                                                                  df.loc[year, (wholesale_control, 'Adjusted_allowed_revenue')] \
+                                                                if year > 2020 and year < 2026 \
+                                                                else 0
+
+            df.loc[year, (wholesale_control, 'Penalty_applicable')] = 1 if df.loc[year, (wholesale_control, 'Forecast_error')] > Minimum_threshold else 0
+
+
+            df.loc[year, (wholesale_control, 'Error_magnitude')] = (df.loc[year, (wholesale_control, 'Forecast_error')] - Minimum_threshold) / \
+                                                                   (Maximum_threshold - Minimum_threshold) \
+                                if df.loc[year, (wholesale_control, 'Penalty_applicable')] == 1 \
                                 else 0
 
-        df.loc[i, 'PR'] = Penalty_rate * min(df.loc[i, 'Error_magnitude'], 1)
+            df.loc[year, (wholesale_control, 'PR')] = Penalty_rate * min(df.loc[year, (wholesale_control, 'Error_magnitude')], 1)
 
-        df.loc[i, 'Penalty_rate_adjustment_POS'] = abs(df.loc[i, 'Revenue_imbalance']) * df.loc[i,'PR'] \
-                                                   if (df.loc[i, 'FY'] > 2020) & (df.loc[i, 'FY'] < 2026) \
-                                                   else 0
+            df.loc[year, (wholesale_control, 'Penalty_rate_adjustment_POS')] = abs(df.loc[year, (wholesale_control, 'Revenue_imbalance')]) * df.loc[year, (wholesale_control, 'PR')] \
+                if year > 2020 and year < 2026 \
+                else 0
 
-        df.loc[i, 'Penalty_rate_adjustment'] = -1 * df.loc[i, 'Penalty_rate_adjustment_POS']
+            df.loc[year, (wholesale_control, 'Penalty_rate_adjustment')] = -1 * df.loc[year, (wholesale_control, 'Penalty_rate_adjustment_POS')]
 
-        df.loc[i, 'Penalty_rate_adjustment_with_financing'] = df.loc[i, 'Penalty_rate_adjustment'] * (1 + discount_rate)
-
-        df.loc[i, 'Penalty_rate_adjustment_with_financing_inflation'] = df.loc[i, 'Penalty_rate_adjustment_with_financing'] *\
-                                                                 (1 + df.loc[i, 'CPIH_delta_shift1']) *\
-                                                                 (1 + df.loc[i, 'CPIH_delta_shift2'])
-
-        df.loc[i, 'Performance_variance_level_alert'] = 1 \
-                                        if df.loc[i, 'Forecast_error'] > Threshold_for_additional_variance_analyses \
-                                        else 0
-
-        df.loc[i+2, 'RFI'] = df.loc[i, 'Main_revenue_adjustment_financing_inflation'] + \
-                           df.loc[i, 'Penalty_rate_adjustment_with_financing_inflation'] \
-                         if (df.loc[i, 'FY'] > 2020) & (df.loc[i, 'FY'] < 2026) \
-                         else 0
+            df.loc[year, (wholesale_control, 'Penalty_rate_adjustment_with_financing')] = df.loc[year, (wholesale_control, 'Penalty_rate_adjustment')] * \
+                                                                                          (1 + discount_rate)
 
 
+            df.loc[year, (wholesale_control, 'Penalty_rate_adjustment_with_financing_inflation')] = df.loc[year, (wholesale_control, 'Penalty_rate_adjustment_with_financing')] * \
+                                                                                                    (1 + df.loc[year, ('All', 'CPIH_delta_shift1')]) * \
+                                                                                                    (1 + df.loc[year, ('All', 'CPIH_delta_shift2')])
+
+            df.loc[year, (wholesale_control, 'Performance_variance_level_alert')] = 1 \
+                if df.loc[year, (wholesale_control, 'Forecast_error')] > Threshold_for_additional_variance_analyses \
+                else 0
+
+            df.loc[year, ('Water_resources', 'Proportion_of_penalty_allocated_to_WaterRes')] = 1 - \
+                                                                                               df.loc[year, ('Water_network', 'Proportion_of_penalty_allocated_to_WaterN')] \
+                if year > 2020 and year < 2026 \
+                else 0
+
+    df.loc[year, ('Water_resources', 'Penalty_rate_adjustment_with_financing_inflation')] = df.loc[year, ('Wholesale_water', 'Penalty_rate_adjustment_with_financing_inflation')] * \
+                                                                                            df.loc[year, ('Water_resources', 'Proportion_of_penalty_allocated_to_WaterRes')]
+
+    df.loc[year, ('Water_network', 'Penalty_rate_adjustment_with_financing_inflation')] = df.loc[year, ('Wholesale_water', 'Penalty_rate_adjustment_with_financing_inflation')] * \
+                                                                                          df.loc[year, ('Water_network', 'Proportion_of_penalty_allocated_to_WaterN')]
+
+    for control in controls:
+        df.loc[year + 2, (control, 'RFI')] = df.loc[year, (control, 'Main_revenue_adjustment_financing_inflation')] + \
+                                             df.loc[year, (control, 'Penalty_rate_adjustment_with_financing_inflation')] \
+                            if year > 2020 and year < 2026 \
+                            else 0
 
 
-print(df.Adjusted_allowed_revenue)
-"""
+df.sort_index(axis=1, inplace=True)
+df.drop([2020, 2026, 2027], inplace=True)
+
+#print(df.loc[:, ('Wastewater_network', 'RFI')])
+
+print(df['Water_resources'].transpose())
+
+print(df[('Water_resources', 'RFI')].transpose())
+
+
+
+
