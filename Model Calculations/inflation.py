45fcf61345be42ad982b957fe7c18542f60f101a
alexwhitmarsh <alexwhitmarsh@yahoo.co.uk>
2020-12-27 08:29:51 +0000

    CPI inflation grab 200824 - with extra series

--- Model Calculations/inflation.py
+++ Model Calculations/inflation.py
@@ -1,51 +1,44 @@
-
-# THIS FILE IS SUPERCEEDED IN SO FAR AS MOST OF IT WAS INCORPORATED INTO RESIDENTIAL RETAIL MODEL
-
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
-
-# The URL syntax is as follows: https://api.ons.gov.uk/dataset/{dataset_id}/timeseries/{timeseries_id}/data
 timseries_ID = 'D7BT'
 dataset_ID = 'MM23'
 URL = 'https://api.ons.gov.uk/dataset/{}/timeseries/{}/data'.format(dataset_ID, timseries_ID)
 r = requests.get(URL)
 
-# Convert to dataframe, create financial years, drop excess data
-df_ONS_raw = pd.DataFrame(r.json()['months'])  # The monthly data is saved in the months part
-df_ONS_raw['FY'] = df_ONS_raw.year.shift(3)  # Shift year column by 3 months to create financial years
-df_ONS_raw.drop(columns=['sourceDataset', 'quarter', 'updateDate', 'label', 'date'],
-                inplace=True)  # Drop superfluous columns
-df_ONS_raw.FY = df_ONS_raw.FY.astype(float)
-df_ONS_raw['value'] = df_ONS_raw.value.astype(float)  # the original data were all strings
-df_ONS_raw = df_ONS_raw.set_index(['FY', 'month'])  # Multi-index for clarity
-print(df_ONS_raw.tail())
+df_CPI = pd.DataFrame(r.json()['months'])  # The monthly data is saved in the months part
+df_CPI['CPI_index'] = df_CPI.value.astype(float)
+df_CPI.drop(columns=['sourceDataset', 'quarter', 'updateDate', 'label',  'value'],
+                inplace=True)
+print(df_CPI)
 
-# Create an empty dataframe to allow space for a forecast period
-df_cpi = pd.DataFrame([], index=range(2009, 2031))
+timseries_ID = 'L522'
+dataset_ID = 'MM23'
+URL = 'https://api.ons.gov.uk/dataset/{}/timeseries/{}/data'.format(dataset_ID, timseries_ID)
+r = requests.get(URL)
 
-# Group (i.e. calculate the FY average) the raw ONS data and merge this into the new dataframe
-s = df_ONS_raw.value.groupby(['FY']).mean()  # This takes the annual average
-df_ONS_grouped = pd.DataFrame(s)  # Converts the new series dataframe to allow subsequent merging
-df_cpi = pd.merge(df_cpi, df_ONS_grouped, how='left', left_on=df_cpi.index, right_on='FY')
+df_CPIH = pd.DataFrame(r.json()['months'])  # The monthly data is saved in the months part
+df_CPIH['CPIH_index'] = df_CPIH.value.astype(float)
+df_CPIH.drop(columns=['sourceDataset', 'quarter', 'updateDate', 'label', 'value'],
+                inplace=True)
+print(df_CPIH)
 
-# FORECAST CPI AND ADD IT INTO THE NEW DATAFRAME
-forecast_cpi = 0.02
-forecast = []
-for i, v in zip(df_cpi.value, df_cpi.FY):
-    if v <= 2019:
-        x = i
-    else:
-        x += x * forecast_cpi
-    forecast.append(x)
-series = pd.Series(forecast, name='CPI_index')
-df_cpi = pd.concat([df_cpi, series], axis=1)
+df = pd.merge(df_CPIH, df_CPI, on=['year', 'month', 'date'])
 
-# CALCULATE THE DIFFERENCE IN THE CPI INDEX
-df_cpi['CPI_delta'] = (df_cpi.CPI_index.diff())  # Take the first difference of the CPI index
-df_cpi['CPI_delta_pct'] = df_cpi.CPI_delta / df_cpi.CPI_index.shift(1)
-print(df_cpi)
+df['CPIH'] = df.CPIH_index.diff()
+df['CPI'] = df.CPI_index.diff()
+df['CPIH_lag'] = df.CPIH.shift(-1)
+df['CPIH_lag2'] = df.CPIH.shift(-2)
+df['CPI_error'] = df.CPI - df.CPIH
+df['CPIH_lag_error'] = df.CPIH_lag - df.CPIH
+df['CPIH_lag_error2'] = df.CPIH_lag2 - df.CPIH
 
 
+print(df)
 
-
+plt.plot(df.date, df.CPIH_lag_error2, label = 'lag2')
+plt.plot(df.date, df.CPIH_lag_error, label = 'lag')
+plt.plot(df.date, df.CPI_error, label = 'CPI')
+plt.legend()
+plt.show()
